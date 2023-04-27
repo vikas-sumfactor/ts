b1e97f3f6b96e9909c325afb236b037d8e779ace
vikas-sumfactor <vikas.sumfactor@gmail.com>
2023-04-27 10:55:23 +0530

    validIpAddress

---
+++ 26Apr2023 Task2/validIpAdress.js
@@ -0,0 +1,7 @@
+function ValidateIPaddress(ipAddress) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress)) {
+        return true;
+    }
+    return false;
+}
+console.log(ValidateIPaddress("192.168.0.1.4"));
---
+++ 26Apr2023 Task2/validIpAdress.ts
@@ -0,0 +1,9 @@
+function ValidateIPaddress(ipAddress:string):boolean 
+{
+ if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipAddress))
+  {
+    return true;
+  }
+return false;
+}
+console.log(ValidateIPaddress("192.168.0.1.4"));
